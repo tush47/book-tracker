e529ffc854b5f97cb1d578416167edeb76f0096f
tush47 <tshulk2003@gmail.com>
2025-08-26 14:20:36 +0530

    render file added

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -1,13 +1,28 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = handler;
 const core_1 = require("@nestjs/core");
 const common_1 = require("@nestjs/common");
 const app_module_1 = require("./app.module");
+const platform_express_1 = require("@nestjs/platform-express");
+const express_1 = __importDefault(require("express"));
+let cachedApp;
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    app.enableCors();
-    app.useGlobalPipes(new common_1.ValidationPipe());
-    await app.listen(process.env.PORT ?? 3000);
+    if (!cachedApp) {
+        const expressApp = (0, express_1.default)();
+        const app = await core_1.NestFactory.create(app_module_1.AppModule, new platform_express_1.ExpressAdapter(expressApp));
+        app.enableCors();
+        app.useGlobalPipes(new common_1.ValidationPipe({ transform: true }));
+        await app.init();
+        cachedApp = expressApp;
+    }
+    return cachedApp;
 }
-bootstrap();
+async function handler(req, res) {
+    const app = await bootstrap();
+    return app(req, res);
+}
 //# sourceMappingURL=main.js.map
